36348f9d199391eb308801a80d145709ec0b6dfd
daanengbers <daan.engbers@gmail.com>
2024-09-03 19:12:19 +0200

    y SORT AND SHIT HYPPEEEEE

--- CMGTjam_Identity/scripts/zelda_camera.gd
+++ CMGTjam_Identity/scripts/zelda_camera.gd
@@ -4,7 +4,7 @@ func _ready():
 	pass
 
 func _process(delta):
-	var pos = get_node("../player").global_position - Vector2(0,0)
+	var pos = get_node("../ySORT/player").global_position - Vector2(0,0)
 	var x = floor(pos.x / 320) * 320
 	position.x = x
 	var y = floor(pos.y / 180) * 180
