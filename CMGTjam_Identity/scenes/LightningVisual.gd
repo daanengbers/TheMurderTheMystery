ffcfd48f1ffd8f0959f0b0cf748eec60ece1cf9e
daanengbers <daan.engbers@gmail.com>
2024-09-03 23:16:26 +0200

    Just a young gun with a quick fuse
    I was uptight, wanna let loose
    I was dreaming of bigger things and
    Wanna leave my own life behind
    Not a yessir. not a follower
    Fit the box, fit the mold
    Have a seat in the foyer, take a number
    I was lighting before the thunder
    
    Thunder, feel the thunder
    Lighting then the thunder
    Thunder, feel the thunder
    Lighting then the thunder
    Thunder. thunder
    Thunder
    
    Kids were laughing in my classes
    While I was scheming for the masses
    Who do you think you are
    Dreaming 'bout being a big star
    They say you're basic, thry say you're easy
    You're always riding in the backseat
    Now I'm smiling from the stage
    While you were clapping in the nose bleeds
    
    Thunder, feel the thunder
    Lighting then the thunder
    Thunder, feel the thunder
    Lighting then the thunder
    Thunder. thunder
    
    Thunder, feel the thunder
    Lighting then the thunder, thunder
    Thunder, feel the thunder
    Lighting then the thunder, thunder
    Thunder, feel the thunder
    Lighting then the thunder, thunder
    Thunder, feel the thunder
    Lighting then the thunder, thunder
    Thunder, feel the thunder
    Lighting then the thunder, thunder

--- CMGTjam_Identity/scenes/LightningVisual.gd
+++ CMGTjam_Identity/scenes/LightningVisual.gd
@@ -15,8 +15,12 @@ func _process(delta):
 
 
 func _on_lightning_timer_2_timeout():
-	print("help")
-	var LightningColor = Color(1, 1, 1)
-	
 	LightningSprite.set_color("#ffffff")
+	$LigthningStop.start()
+	pass # Replace with function body.
+	
+
+
+func _on_ligthning_stop_timeout():
+	LightningSprite.set_color("#ffffff00")
 	pass # Replace with function body.
