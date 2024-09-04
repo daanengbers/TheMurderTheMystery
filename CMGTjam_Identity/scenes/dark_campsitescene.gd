70e5d03368317ca1e1625a735ad9fea0736efcbc
WeenterMakesGames <weentertheperson@gmail.com>
2024-09-05 01:40:38 +0200

    darkworld filter

--- CMGTjam_Identity/scenes/dark_campsitescene.gd
+++ CMGTjam_Identity/scenes/dark_campsitescene.gd
@@ -2,4 +2,12 @@ extends Node2D
 
 func _ready():
 	GlobalDialogue.fadeIn()
+	
 	#GlobalDialogue.setDialougueOff()
+	
+	
+	
+	# play all the floating animations -----
+	$Axe/Anim.play("float")
+	$ySORT/Tent9/Anim.play("float")
+	$ySORT/Tent12/Anim.play("float")
