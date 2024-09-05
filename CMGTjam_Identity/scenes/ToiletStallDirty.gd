a65a6cf346c901601e1fe95add0da7166f996966
daanengbers <daan.engbers@gmail.com>
2024-09-05 02:26:07 +0200

    Interior are now supertof

---
+++ CMGTjam_Identity/scenes/ToiletStallDirty.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Toilet/AnimationPlayer.play("TopSincoToilet")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
